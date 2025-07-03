889a659a659ee70018d5c0b8967c969049d05100
Breno Felix Gomes Silva. <50252782+DeusdosSayajins@users.noreply.github.com>
2025-07-03 09:39:47 -0300

    Update app.js

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,14 +1,14 @@
 alert ('Boas Vindas');
-//let nome = 'Lua';
-//let idade = 25;
+let nome = 'Lua';
+let idade = 25;
 let numeroDeVendas = 50;
 let saldoDisponivel = 1000;
 //alert('Preencha Todos os Campos');
 let menssagemErro = 'Erro! Preencha todos os campos';
 alert(menssagemErro);
-let nome = prompt('Digite um nome!');
-let idade = prompt('Digite sua idade!');
-if(idade >= 18){
+let nome2 = prompt('Digite um nome!');
+let idadeHabilitacao = prompt('Digite sua idade!');
+if(idadeHabilitacao >= 18){
     alert('Você é maior de idade e pode tirar a Habilitação');
 }   else{
     alert('Você é menor de idade, espere completar 18, para tirar a Habilitação');
